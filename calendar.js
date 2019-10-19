c3927702c07f85bfa3d59dae6448f678882d7e92
Ctucker9233 <court.m.tucker@gmail.com>
2019-10-19 12:05:03 -0700

    got it to store each day's task in localStorage and retrieve it

--- calendar.js
+++ calendar.js
@@ -1,71 +1,102 @@
-//get current day
-var todayEl = document.querySelector('.today');
-var containerEl = document.querySelector('.container')
-var colElHour;
-var inputEl;
-var hoursPerDay = 24;
+let todayEl = $('.today');
+let containerEl = $('.container')
+var rowEl;
+var hoursPerDay = 9;
+var currentHour;
 var time = [];
+let colElHour;
+let inputEl;
+let saveBtn;
+let colElSave;
+let colElDesc;
 var index = [];
 
-console.log(moment().format('MM/DD/YYYY'));
-todayEl.textContent = "Today is " + moment().format('MM/DD/YYYY') + ".";
-//get the number of hours in a day
+//get current day
+//console.log(moment().format('MM/DD/YYYY'));
+todayEl.text("Today is " + moment().format('MM/DD/YYYY') + ".");
+//console.log(moment().startOf('day').add('8', 'hours').format("h:mmA"))
 
+//get the number of hours in a work day
 function timeOneDay() {
-    var formattedTime;
+    var formattedTime = [];
+    time = formattedTime;
     for (i = 0; i < hoursPerDay; i++) {
-        formattedTime = (moment().startOf('day').subtract(i, "hours")).format("hA");
-        time.unshift(formattedTime);
+        var timePoint = (9 + i);
+        formattedTime.push((moment().startOf('day').add(timePoint, 'hours').format("HH:mm A")));
     }
+
 }
+
 //render calendar
 function renderCalendar() {
     for (i = 0; i < hoursPerDay; i++) {
-        var rowEl = document.createElement('div');
-        rowEl.setAttribute("class", "row");
-        colElHour = document.createElement('div');
-        colElHour.setAttribute("class", "col-lg-1 hour")
-        colElHour.textContent = time[i];
-        var colElDesc = document.createElement('div');
-        colElDesc.setAttribute("class", "col-lg-10")
-        inputEl = document.createElement('input');
-        inputEl.setAttribute('type', "textarea")
-        inputEl.setAttribute("class", "description")
-        inputEl.setAttribute("id", i)
-        index.push(parseInt(inputEl.getAttribute("id")));
-        var colElSave = document.createElement('div')
-        colElSave.setAttribute("class", "col-lg-1");
-        var saveBtn = document.createElement('button');
-        saveBtn.setAttribute("class", "saveBtn i");
-        saveBtn.setAttribute("type", "submit");
-        saveBtn.textContent = "Save";
-        colElSave.appendChild(saveBtn);
-        colElDesc.appendChild(inputEl)
-        rowEl.appendChild(colElHour);
-        rowEl.appendChild(colElDesc);
-        rowEl.appendChild(colElSave);
-        containerEl.appendChild(rowEl);
+        rowEl = $('<div>').attr("class", "row");
+        colElHour = $('<div>' + time[i] + '</div>').attr("class", "col-lg-1 hour");
+        colElDesc = $('<div>').attr("class", "col-lg-10");
+        inputEl = $('<textarea>').attr('value', "").attr("class", "description").attr("id", time[i]);
+        index.push(inputEl.attr("id"));
+        colElSave = $('<div>').attr("class", "col-lg-1")
+        saveBtn = $('<button>').attr("class", "saveBtn").attr("id", time[i]).attr("type", "submit").text("Save");
+        colElDesc.append(inputEl)
+        colElSave.append(saveBtn);
+        rowEl.append(colElHour);
+        rowEl.append(colElDesc);
+        rowEl.append(colElSave);
+        containerEl.append(rowEl);
     }
+    $(document).on("click", ".saveBtn", saveSchedule);
 }
 
+//determine current time
 function currentTime() {
-    var currentHour = parseInt(moment().hour());
-    console.log(currentHour);
-    console.log(index);
-    for (i = 0; i < index.length; i++) {
-        var timeSlot = document.getElementById(i);
-        console.log(timeSlot);
-        if ((currentHour - 1) === index[i])  {
-            timeSlot.setAttribute("class", "description present")
+    //console.log(moment().format("HH:mm A"));
+    currentHour = moment().format("HH:mm A");
+    //console.log(currentHour);
+    var timeStamp = $('textarea').get();
+    var getSched;
+    $.each(timeStamp, function (i) {
+        //console.log(timeStamp[i]);
+        //console.log(parseInt(currentHour));
+        var timeId = parseInt(timeStamp[i].id);
+        //console.log(parseInt(currentHour));
+        //console.log(timeStamp[i].classList.value)
+        console.log(timeStamp[i])
+        if (parseInt(currentHour) > timeId) {
+            //console.log("Past Time " + timeId)
+            //console.log(timeStamp.classList)
+            timeStamp[i].classList.value = "description past";
+            //
+            getSched = localStorage.getItem(timeStamp[i].id)
+            console.log(getSched);
+            timeStamp[i].value = getSched;
         }
-        else if ((currentHour - 1) > index[i]){
-            timeSlot.setAttribute("class", "description past")
+        else if (parseInt(currentHour) < timeId) {
+            //console.log("Past Time " + time[i])
+            timeStamp[i].classList.value = "description future";
+            getSched = localStorage.getItem(timeStamp[i].id)
+            console.log(getSched);
+            timeStamp[i].value = getSched;
         }
         else {
-            timeSlot.setAttribute("class", "description future")
+            console.log("future Time " + parseInt(timeStamp[i]))
+            timeStamp[i].classList.value = "description present";
+            getSched = localStorage.getItem(timeStamp[i].id)
+            console.log(getSched);
+            timeStamp[i].value = getSched;
         }
-    }
+    })
+};
+
+function saveSchedule(event) {
+    event.preventDefault()
+    var input = $(this).parent().parent().find(".description", ['textarea'])[0].value
+    var time = $(this).parent().parent().find(".description", ['textarea'])[0].id;
+    console.log(time)
+    console.log(input)
+    localStorage.setItem(time, input)
 }
+
+
 timeOneDay();
 renderCalendar();
 console.log(time);
